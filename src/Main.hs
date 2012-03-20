023678c3afac7009e677bc60c9cbcee1b6f8f337
Dustin Rohde <dtrohde@fastmail.net>
2012-03-20 16:48:47 -0400

    First commit.
    The game appears to be working, but it needs test coverage. An executable
    compiled by ghc on my i686 Linux system is included for convenience.

---
+++ src/Main.hs
@@ -0,0 +1,106 @@
+module Main where
+
+import Control.Monad (sequence_)
+import Data.Char (isSpace)
+import Data.List (intercalate)
+import qualified Data.Map as Map (member, notMember, lookup)
+import Data.Maybe (fromJust)
+import System.IO (hFlush, stdout)
+import TicTacToe
+
+
+main :: IO ()
+main = do
+    putStrLn "<< TIC TAC TOE >>\n"
+    nameX <- promptForName "Player X"
+    nameO <- promptForName "Player O"
+    presentInstructions
+    mainLoop nameX nameO newGame
+
+
+presentInstructions :: IO ()
+presentInstructions = do
+    putStrLn "\nTake turns making moves until someone gets a 3-in-a-row."
+    putStrLn "Enter each move as two digits separated by a space."
+    prompt "Possible coordinates are 0, 1 and 2.\n"
+    return ()
+
+
+mainLoop :: PlayerName -> PlayerName -> Game -> IO ()
+mainLoop nameX nameO game = do
+    drawGame game
+    g'@(Game _ statusX) <- takePlayerTurn X nameX nameO game
+    case statusX of
+        Winner nameX -> gameOver nameX nameO g'
+        Undecided    -> do
+            drawGame g'
+            g''@(Game _ statusO) <- takePlayerTurn O nameO nameX g'
+            case statusO of
+                Winner nameO -> gameOver nameO nameX g''
+                Undecided    -> mainLoop nameX nameO g''
+
+
+takePlayerTurn :: Mark -> PlayerName -> PlayerName -> Game -> IO Game
+takePlayerTurn mark playing waiting g@(Game plays end) = do
+    point <- promptForPlay playing
+    if isValidPlay point g then do
+        describePlay playing point
+        let g'@(Game plays' _) = makePlay mark point g
+        if ticTacToe mark point g' then
+            return $ Game plays' (Winner playing)
+        else
+            return g'
+    else do
+        putStrLn $ "Invalid move." 
+        takePlayerTurn mark playing waiting g
+
+
+gameOver :: PlayerName -> PlayerName -> Game -> IO ()
+gameOver winner loser game = do
+    drawGame game
+    putStrLn $ winner ++ " wins! Better luck next time, " ++ loser ++ "."
+    return ()
+        
+
+prompt :: String -> IO String
+prompt st = do
+    putStr st
+    hFlush stdout
+    getLine
+
+
+promptForName :: String -> IO String
+promptForName playerSt = prompt $ playerSt ++ ", what is your name? "
+
+
+promptForPlay :: PlayerName -> IO Point
+promptForPlay playerName = do
+    input <- prompt ('\n':playerName ++ "'s move: ")
+    case input of
+        (x:' ':y:[]) -> parseMove input
+        otherwise    -> do
+            putStrLn "Invalid input. Try again."
+            promptForPlay playerName
+    where parseMove coordStr = do
+              let xy = break isSpace coordStr
+              return $ Point (read (fst xy)) (read (snd xy))
+
+
+describePlay :: PlayerName -> Point -> IO ()
+describePlay name (Point x y) =
+    putStrLn $ name ++ " plays at (" ++ show x ++ ", " ++ show y ++ ").\n"
+
+
+drawGame :: Game -> IO ()
+drawGame game = do
+        sequence_ $ intercalate [putChar '\n']
+                                [[drawCell x y | x <- [0..2]] | y <- [0..2]]
+        putStrLn ""
+        return ()
+    where drawCell x' y' = do putStr (getCellStr (Point x' y') game)
+
+
+getCellStr :: Point -> Game -> String
+getCellStr point game = if point `played` game
+                        then show . fromJust . markAt point $ game 
+                        else "."
---
+++ src/TicTacToe.hs
@@ -0,0 +1,108 @@
+module TicTacToe
+-- * Data types
+( Game(..)
+, Point(..)
+, Mark(..)
+, EndStatus(..)
+, PlayerName
+, Direction(..)
+-- * Core functions
+, newGame
+, makePlay
+, ticTacToe
+-- * Supporting functions
+, threeInARow
+, inBounds
+, isValidPlay
+-- * Data.Map function aliases
+, markAt        -- ^ lookup
+, played        -- ^ member 
+, notPlayed     -- ^ notMember
+) where
+
+import Data.Map (Map, empty, member, notMember, insert, fromList, size)
+import qualified Data.Map as Map (lookup)
+
+
+-- |A game is a mapping of points to marks and an end status.
+data Game = Game { getPlays :: (Map Point Mark)
+                 , getStatus :: EndStatus }
+                 deriving (Show)
+
+-- |A point is two coordinates.
+data Point = Point Int Int
+             deriving (Show, Eq, Ord)
+
+-- |A mark is either X or O.
+data Mark = X | O
+            deriving (Show, Eq, Ord)
+
+-- |An end status describes whether a game is over, and how.
+data EndStatus = Undecided | Winner PlayerName
+                  deriving (Show, Eq) 
+
+type PlayerName = String
+
+-- |A direction is a cardinal or ordinal point.
+data Direction = N | NE | E | SE | S | SW | W | NW
+                 deriving (Show, Eq, Enum)
+
+
+newGame :: Game
+newGame = Game empty Undecided
+
+
+makePlay :: Mark -> Point -> Game -> Game
+makePlay mark point g@(Game plays end) = if inBounds point
+                                         then Game (insert point mark plays) end
+                                         else g
+
+
+inBounds :: Point -> Bool
+inBounds (Point x y) = x >= 0 && x < 3
+                    && y >= 0 && y < 3
+
+
+isValidPlay :: Point -> Game -> Bool
+isValidPlay point game = inBounds point && notPlayed point game
+
+
+ticTacToe :: Mark -> Point -> Game -> Bool
+ticTacToe mark p game = or [threeInARow mark p dir game | dir <- [N .. NW]]
+
+
+threeInARow :: Mark -> Point -> Direction -> Game -> Bool
+threeInARow mark (Point x y) dir game = if all inBounds points
+                                                  then all (== Just mark) $ marks
+                                                  else False
+    where marks  = map (`markAt` game) points 
+          points = let x1 = if centerX then x-dx else x+dx
+                       y1 = if centerY then y-dy else y+dy
+                       x2 = if centerX then x+dx else x1+dx
+                       y2 = if centerY then y+dy else y1+dy
+                       centerX  = x == 1
+                       centerY  = y == 1
+                       (dx, dy) = dirToXY dir
+                   in [Point x y, Point x1 y1, Point x2 y2]
+
+dirToXY :: Direction -> (Int, Int)
+dirToXY N  = (0, 1)
+dirToXY NE = (1, 1)
+dirToXY E  = (1, 0)
+dirToXY SE = (1, -1)
+dirToXY S  = (0, -1)
+dirToXY SW = (-1, -1)
+dirToXY W  = (-1, 0)
+dirToXY NW = (-1, 1)
+
+
+markAt :: Point -> Game -> Maybe Mark
+markAt point (Game plays _) = Map.lookup point plays
+
+
+played :: Point -> Game -> Bool
+point `played` (Game plays _) = point `member` plays
+
+
+notPlayed :: Point -> Game -> Bool
+point `notPlayed` (Game plays _) = point `notMember` plays
